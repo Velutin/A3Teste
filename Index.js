b4a123c60890db0849b8ea62ed76e347fc7f513f
Velutin <107081270+Velutin@users.noreply.github.com>
2025-05-29 00:30:27 -0300

    Initial commit

---
+++ Index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const JogoController = require("./controllers/JogoController");
+const EmpresaController = require("./controllers/EmpresaController");
+
+const app = express();
+const APP_PORT = process.env.APP_PORT || 3000;
+
+app.use(express.json());
+
+app.listen(APP_PORT, () =>{
+    console.log(`API de jogo em execução na porta ${APP_PORT}.`);
+    console.log(`Acesse a url http://localhost:${APP_PORT}`);
+});
+
+app.get("/",(req,res) => res.send("API Version 1.1.0 on-line!"));
+
+app.get("/jogos",JogoController.index);
+
+app.get("/jogos/:id",JogoController.show);
+
+app.post("/jogos",JogoController.create);
+
+app.put("/jogos/:id",JogoController.update);
+
+app.delete("/jogos/:id",JogoController.delete);
+
+app.get("/empresas",EmpresaController.index);
+app.get("/empresas/:id",EmpresaController.show);
+app.post("/empresas",EmpresaController.create);
+
+app.put("/empresas/:id",EmpresaController.update);
+
+app.delete("/empresas/:id",EmpresaController.delete);   
